d85c1c1aab19e42ea7f9ccb5e9364121c4e99c15
jaeho.lee <jaeho.lee@barunn.net>
2024-03-19 15:09:18 +0900

    공지사항 추가

---
+++ Controllers/NoticeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToyProject.DBContext;
+using ToyProject.Models;
+
+namespace ToyProject.Controllers
+{
+    public class NoticeController : BaseController
+    {
+        public NoticeController(ILogger<NoticeController> logger, IameeoContext iameeoDB) : base(logger, iameeoDB) 
+        {
+            _iameeoDB.Notices.Add(new Notice
+            {
+                UserId = "jaeho.lee",
+                Name = "이주원",
+                Regdate = DateTime.Now
+            });
+            _iameeoDB.SaveChanges();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
