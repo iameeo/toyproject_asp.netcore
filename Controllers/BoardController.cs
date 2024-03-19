6cfc5a70c404f15e07a8689d6aaf106339e80773
jaeho.lee <jaeho.lee@barunn.net>
2024-03-19 13:44:00 +0900

    게시판 CRUD 스캐폴드용으로 생성

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToyProject.DBContext;
+using ToyProject.Models;
+
+namespace ToyProject.Controllers
+{
+    public class BoardController : BaseController
+    {
+        public BoardController(ILogger<BoardController> logger, IameeoContext iameeoDB) : base(logger, iameeoDB)
+        {
+
+        }
+
+        // GET: Board
+        public async Task<IActionResult> Index()
+        {
+            return View(await _iameeoDB.Board.ToListAsync());
+        }
+
+        // GET: Board/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _iameeoDB.Board
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // GET: Board/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Board/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,Name,Regdate")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                _iameeoDB.Add(board);
+                await _iameeoDB.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Board/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _iameeoDB.Board.FindAsync(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Board/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Name,Regdate")] Board board)
+        {
+            if (id != board.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _iameeoDB.Update(board);
+                    await _iameeoDB.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoardExists(board.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Board/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _iameeoDB.Board
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // POST: Board/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var board = await _iameeoDB.Board.FindAsync(id);
+            if (board != null)
+            {
+                _iameeoDB.Board.Remove(board);
+            }
+
+            await _iameeoDB.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoardExists(int id)
+        {
+            return _iameeoDB.Board.Any(e => e.Id == id);
+        }
+    }
+}
